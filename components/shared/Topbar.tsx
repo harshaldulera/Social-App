c08de42be7d6756cf94d1dace8b9119f1d5f0ce3
Harshal Dulera <harshaldulera82@gmail.com>
2023-10-29 07:03:17 +0530

    topbar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -5,6 +5,9 @@ function Topbar () {
         <nav className="topbar">
             <Link href="/" className="flex item-center gap-4">
                 {/* <Image src="/logo.svg" alt="logo" width={28} height={28} /> */}
+                <p className="text-heading3-bold text-light-1 max-xs:hidden">
+                    Threads
+                </p>
             
             </Link>
 
