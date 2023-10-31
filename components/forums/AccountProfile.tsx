6ba8feb0311f60ba0ffb17dd22cbbff573e67a30
Harshal Dulera <harshaldulera82@gmail.com>
2023-10-31 23:11:22 +0530

    config changes

--- components/forums/AccountProfile.tsx
+++ components/forums/AccountProfile.tsx
@@ -36,10 +36,10 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
     const form = useForm({
         resolver: zodResolver(UserValidation),
         defaultValues: {
-            profile_photo: '',
-            name: '',
-            username: '',
-            bio: ''
+            profile_photo: user?.image || "",
+            name: user?.name || "",
+            username: user?.username || "",
+            bio: user?.bio || ""
         }
     })
 
@@ -150,7 +150,9 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                     </FormItem>
                 )}
             />
-            <Button type="submit" className='bg-primary-500'>Submit</Button>
+            <Button type="submit" className='bg-primary-500'>
+                {btnTitle}
+            </Button>
             </form>
         </Form>
     )
