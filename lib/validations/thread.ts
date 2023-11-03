f29f372790581aab69ce09c160618ef0f8ac03cc
Harshal Dulera <harshaldulera82@gmail.com>
2023-11-03 11:14:08 +0530

    Thread model

--- lib/validations/thread.ts
+++ lib/validations/thread.ts
@@ -5,7 +5,7 @@ export const ThreadValidation = z.object({
     accountId: z.string(),
 })
 
-export const ThreadValidation = z.object({
-    thread: z.string().nonempty().min(3, { message: 'Minimum 3 Characters' }),
-    accountId: z.string(),
-})
+// export const ThreadValidation = z.object({
+//     thread: z.string().nonempty().min(3, { message: 'Minimum 3 Characters' }),
+//     accountId: z.string(),
+// })
