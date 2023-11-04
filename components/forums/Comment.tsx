63cd02b15301b29f274d54832212738c7292ec29
Harshal Dulera <harshaldulera82@gmail.com>
2023-11-04 09:58:00 +0530

    Comment Page

--- components/forums/Comment.tsx
+++ components/forums/Comment.tsx
@@ -1,8 +1,75 @@
+import { useForm } from "react-hook-form";
+import { Button } from '@/components/ui/button';
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+}  from '@/components/ui/form'
+import * as z from "zod";
+import { Textarea } from '@/components/ui/textarea'
+import { zodResolver } from '@hookform/resolvers/zod';
+import { usePathname, useRouter } from 'next/navigation';
+
+interface Props {
+    threadId: string;
+    currentUserImg: string;
+    currentUserId: string;
+}
+
 const Comment = ({ threadId, currentUserImg, currentUserId}: Props) => {
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const form = useForm({
+        resolver: zodResolver(ThreadValidation),
+        defaultValues: {
+            thread: '',
+            accountId: userId,
+        },
+    });
+
+    const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
+        await createThread({ 
+            text: values.thread,
+            author: userId,
+            communityId: null,
+            path: pathname,
+        });
+
+        router.push("/");
+    }
+
     return (
-        <div>
-            <h1 className="text-white">Comment Form</h1>
-        </div>
+        <Form {...form}>
+            <form
+                className='mt-10 flex flex-col justify-start gap-10'
+                onSubmit={form.handleSubmit(onSubmit)}
+            >
+            <FormField
+                control={form.control}
+                name='thread'
+                render={({ field }) => (
+                    <FormItem className='flex w-full flex-col gap-3'>
+                        <FormLabel className='text-base-semibold text-light-2'>
+                        Content
+                        </FormLabel>
+                        <FormControl className='no-focus border border-dark-4 bg-dark-3 text-light-1'>
+                            <Textarea rows={15} {...field} />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                )}
+            />
+    
+            <Button type='submit' className='bg-primary-500'>
+                Post Thread
+            </Button>
+            </form>
+        </Form>    
     )
 }
 
