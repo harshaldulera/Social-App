6114617957ab35ca94ab91f37b7a9969e97d54e0
Harshal Dulera <harshaldulera82@gmail.com>
2023-11-06 11:16:38 +0530

    Profile Header

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,7 +1,27 @@
-const Page = async () => {
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const Page = async ({ params }: { params: { id: string }}) => {
+    const user = await currentUser();
+
+    if(!user) return null;
+
+    const userInfo = await fetchUser(params.id);
+
+    if(!userInfo?.onboarded) redirect('/onboarding');
+
     return (
         <section>
-            Profile
+            <ProfileHeader
+                accountId={userInfo.id}
+                authUserId={user.id}
+                name={userInfo.name}
+                username={userInfo.username}
+                imgUrl={userInfo.image}
+                bio={userInfo.bio}
+            />
         </section>
     )
 }
