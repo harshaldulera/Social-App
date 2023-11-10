da5a45b8142379edd231d3b9947e4feebbc7d85a
Harshal Dulera <harshaldulera82@gmail.com>
2023-11-10 23:58:45 +0530

    get activity actions

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,4 +1,14 @@
-const page = async () => {
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs"
+import { redirect } from "next/navigation";
+
+async function page() {
+    const user = await currentUser();   
+    if(!user) return null;
+
+    const userInfo = await fetchUser(user.id);
+    if(!userInfo?.onboarded) redirect("/onboarding");
+
     return (
         <section>
             <h1 className="head-text mb-10">Activity</h1>
