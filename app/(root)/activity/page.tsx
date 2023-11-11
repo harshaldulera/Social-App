73404228bc244b6bdcd315e64ee1e9599be7282f
Harshal Dulera <harshaldulera82@gmail.com>
2023-11-11 20:28:32 +0530

    activity page

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -29,6 +29,12 @@ async function page() {
                                         height={20}
                                         className="rounded-full object-cover"
                                     />
+                                    <p className="!text-small-regular text-light-1">
+                                        <span className="mr-1 text-primary-500">
+                                            {activity.author.name}
+                                        </span>{" "}
+                                        replied to your thread
+                                    </p>
                                 </article>
                             </Link>
                         ))}
