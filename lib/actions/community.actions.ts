c5e6dfefb0e5a19392346f5d43b6e63e5956f357
Harshal Dulera <harshaldulera82@gmail.com>
2023-11-13 10:12:41 +0530

    fetch communities action

--- lib/actions/community.actions.ts
+++ lib/actions/community.actions.ts
@@ -97,3 +97,51 @@ export async function fetchCommunityPosts(id: string) {
         throw error;
     }
 }
+
+export async function fetchCommunities({
+    searchString = "",
+    pageNumber = 1,
+    pageSize = 20,
+    sortBy = "desc",
+} : {
+    searchString?: string;
+    pageNumber?: number;
+    pageSize?: number;
+    sortBy?: SortOrder;
+}) {
+    try {
+        connectToDB();
+
+        const skipAmount = (pageNumber - 1) * pageSize;
+
+        const regex = new RegExp(searchString, "i");
+
+        const query: FilterQuery<typeof Community> = {};
+        
+        if (searchString.trim() !== "" ) {
+            query.$or = [
+                { username: { $regex: regex } },
+                { name: {  $regex: regex } },
+            ];
+        }
+
+        const sortOptions = { createdAt: sortBy };
+
+        const communitiesQuery = Community.find(query)
+            .sort(sortOptions)
+            .skip(skipAmount)
+            .limit(pageSize)
+            .populate("members");
+
+        const totalCommunitiesCoumt = await Community.countDocuments(query);
+
+        const communities = await communitiesQuery.exec();
+
+        const isNext = totalCommunitiesCoumt > skipAmount + communities.length;
+
+        return { communities, isNext };
+    } catch (error) {
+        console.error("Error fetching Communities: ", error);
+        throw error;
+    }
+}
